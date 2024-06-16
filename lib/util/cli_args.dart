4e7ad90b085bdde896f82e99e2ca65b114c4fbff
ZXED <zxed@alkatrazstudio.net>
2024-06-16 13:44:15 +0300

    Always work with absolute paths for args

--- lib/util/cli_args.dart
+++ lib/util/cli_args.dart
@@ -1,12 +1,16 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 
 import 'package:args/args.dart';
+import 'package:path/path.dart';
 
 class CliArgs {
   CliArgs(List<String> args) {
     var parser = ArgParser();
     var results = parser.parse(args);
-    path = results.rest.firstOrNull;
+    var argPath = results.rest.firstOrNull;
+    if(argPath != null)
+      argPath = absolute(argPath);
+    path = argPath;
   }
 
   late final String? path;
